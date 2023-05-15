253a57e078c2ca4f1653b0a0faae580236950382
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2023-05-15 13:30:54 +0200

    Updated help command

--- response-cases/help.py
+++ response-cases/help.py
@@ -1,6 +1,12 @@
 def handle_help() -> str:
     response = "Stay awhile and listen! I am Deckard Cain, your personal guide to Diablo 4. Here are some commands you can use:\n\n"
     response += "`!help`: Displays this list of commands.\n"
+    response += "`!hello`: Say hello.\n"
     response += "`!lore`: Shares a random piece of Diablo 4 lore with you.\n"
-    response += "\nIf you have any questions or need help, just type `!help` again and I'll be happy to assist you!"
+    response += "`!countdown <event>`: Shows the remaining time until chosen event.\n"
+    response += "`!gpt <prompt>`: Talk to CainGPT!\n"
+    response += (
+        "`!add <key type> <api key>`: Register your API key to my secret JSON file.\n"
+    )
+    response += "\nIf you have any questions or need help, just type `!help` again and I'll be happy to assist you! Remember that you can always send me a direct message."
     return response
